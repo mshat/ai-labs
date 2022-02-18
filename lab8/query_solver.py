ee4608808bdf62b5529bd4d045264a5f9022e5bf
Maksim Shatokhin <max.sh99@mail.ru>
2022-02-19 02:11:44 +0300

    переписал паттерны solver-а в соответствии с новыми полями Word. Вынес группы паттернов в методы. Добавил декоратор debug_print

--- lab8/query_solver.py
+++ lab8/query_solver.py
@@ -2,6 +2,20 @@ import enum
 from query import Query
 
 
+DEBUG = True
+
+
+def debug_print(msg: str):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            if DEBUG:
+                print(f'[DEBUG] {msg}')
+            function(*args, **kwargs)
+
+        return wrapper
+    return decorator
+
+
 class DialogState(enum.Enum):
     start = 1
     search = 2
@@ -15,85 +29,108 @@ class DialogState(enum.Enum):
 
 class QuerySolver:
     def __init__(self):
-        self.state = DialogState.start
+        self._state = DialogState.start
         # self.state = DialogState.search
         self.response = None
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, val):
+        self._state = val
 
     def _get_arguments_by_type(self, query: Query, argument_type: str):
         return [argument for argument_type, arguments in query.arguments.items() for argument in arguments if argument_type == argument_type]
 
+    @debug_print('Рестарт')
     def restart(self):
         self.state = DialogState.start
         self.response = None
-        print('рестарт')
 
+    @debug_print('Поставить лайк')
     def like(self, query: Query):
+        self.state = DialogState.like
         allowed_states = (DialogState.search, DialogState.start)
         artists = self._get_arguments_by_type(query, 'ArtistArgument')
-        print(f'лайк: {artists}')
 
+    @debug_print('Поставить дизлайк')
     def dislike(self, query: Query):
+        self.state = DialogState.dislike
         allowed_states = (DialogState.search, DialogState.start)
         artists = self._get_arguments_by_type(query, 'ArtistArgument')
-        print(f'дизлайк: {artists}')
 
+    @debug_print('Вывести всех артистов в базе')
     def show_all_artists(self, query: Query):
+        self.state = DialogState.search
         allowed_states = (DialogState.filter, DialogState.count_filter, DialogState.start)
-        print(f'вывести всех артистов в базе')
 
+    @debug_print('Рекомендация по артисту')
     def search_by_artist(self, query: Query):
+        self.state = DialogState.search
         allowed_states = (DialogState.filter, DialogState.count_filter, DialogState.start)
         artists = self._get_arguments_by_type(query, 'ArtistArgument')
-        print(f'поиск по артисту: {artists}')
 
+    @debug_print('Вывести артистов в определённом жанре')
     def search_by_genre(self, query: Query):
+        self.state = DialogState.search
         allowed_states = (DialogState.filter, DialogState.count_filter, DialogState.start)
         genres = self._get_arguments_by_type(query, 'GenreArgument')
-        print(f'поиск по жанру: {genres}')
 
+    @debug_print('Вывести исполнителей указанного пола')
     def search_by_sex(self, query: Query):
+        self.state = DialogState.search
         allowed_states = (DialogState.filter, DialogState.count_filter, DialogState.start)
         sex = self._get_arguments_by_type(query, 'SexArgument')[0]
-        print(f'вывести исполнителей {sex} пола')
 
+    @debug_print('Информация об артисте')
     def info(self, query: Query):
+        self.state = DialogState.info
         allowed_states = (DialogState.filter, DialogState.count_filter, DialogState.start)
         artists = self._get_arguments_by_type(query, 'ArtistArgument')
-        print(f'информация об артисте: {artists}')
 
+    @debug_print('Количество артистов в базе')
     def number(self, query: Query):
+        self.state = DialogState.number
         allowed_states = (DialogState.number, DialogState.search, DialogState.start)
         print(query.arguments.items())
-        print('количество артистов в базе')
 
+    @debug_print('Количество артистов указанного пола в базе')
     def number_with_sex(self, query: Query):
+        self.state = DialogState.number
         allowed_states = (DialogState.number, DialogState.search, DialogState.start)
         sex = self._get_arguments_by_type(query, 'SexArgument')[0]
-        print(f'количество артистов {sex} пола в базе')
 
+    @debug_print('Количество артистов указанного пола в базе')
     def number_with_age(self, query: Query):
+        self.state = DialogState.number
         allowed_states = (DialogState.number, DialogState.search, DialogState.start)
         age = self._get_arguments_by_type(query, 'NumArgument')
-        if len(age) == 2:
-            from_age, to_age = sorted([int(age[0].value), int(age[1].value)])
-            print(f'количество артистов от {from_age} до {to_age} лет')
-        else:
-            if 'younger' in query.tags_query_structure:
-                print(f'количество артистов до {age} лет')
-            elif 'older' in query.tags_query_structure:
-                print(f'количество артистов от {age} лет')
+        # if len(age) == 2:
+        #     from_age, to_age = sorted([int(age[0].value), int(age[1].value)])
+        #     print(f'количество артистов от {from_age} до {to_age} лет')
+        # else:
+        #     if 'younger' in query.tags_query_structure:
+        #         print(f'количество артистов до {age} лет')
+        #     elif 'older' in query.tags_query_structure:
+        #         print(f'количество артистов от {age} лет')
 
+    @debug_print('Изменить количество выводимых результатов')
     def set_result_len(self, query: Query):
+        self.state = DialogState.count_filter
         allowed_states = (DialogState.filter, DialogState.start)
         result_len = self._get_arguments_by_type(query, 'NumArgument')
-        print(f'размер выборки: {result_len}')
 
+    @debug_print('Фильтр по полу')
     def filter_by_sex(self, query: Query):
+        self.state = DialogState.filter
         allowed_states = (DialogState.filter, DialogState.start)
         sex = self._get_arguments_by_type(query, 'SexArgument')[0]
-        print(f'фильтр по полу: {sex}')
 
+    @debug_print('Фильтр по возрасту')
     def filter_by_age_include(self, query: Query):
+        self.state = DialogState.filter
         allowed_states = (DialogState.filter, DialogState.start)
         age = self._get_arguments_by_type(query, 'NumArgument')
         if len(age) == 2:
@@ -105,7 +142,9 @@ class QuerySolver:
             elif 'older' in query.tags_query_structure:
                 print(f'фильтр от {age} лет')
 
+    @debug_print('Фильтр по возрасту')
     def filter_by_age_exclude(self, query: Query):
+        self.state = DialogState.filter
         allowed_states = (DialogState.filter, DialogState.start)
         age = self._get_arguments_by_type(query, 'NumArgument')
         if len(age) == 2:
@@ -117,7 +156,9 @@ class QuerySolver:
             elif 'older' in query.tags_query_structure:
                 print(f'фильтр до {age} лет')
 
+    @debug_print('Фильтр по количеству участников коллектива')
     def filter_by_members_count(self, query: Query):
+        self.state = DialogState.filter
         allowed_states = (DialogState.filter, DialogState.start)
         tags = query.tags_query_structure
         if 'group' in tags:
@@ -128,107 +169,124 @@ class QuerySolver:
             members_count_type = 'duet'
         print(f'оставить {members_count_type}')
 
+    @debug_print('Удалить ограничение количества выводимых строк')
     def remove_result_len_filter(self, query):
+        self.state = DialogState.filter
         allowed_states = (DialogState.filter, DialogState.start)
-        print('выводить всю выборку без ограничения на количество')
 
-    def remove_age_filter(self, query):
-        allowed_states = (DialogState.filter, DialogState.start)
-        print('удалить фильтр возраста')
-
-    def remove_sex_filter(self, query):
-        allowed_states = (DialogState.filter, DialogState.start)
-        print('удалить фильтр пола')
-
-    def remove_members_count_filter(self, query):
-        allowed_states = (DialogState.filter, DialogState.start)
-        print('удалить фильтр количества артистов')
-
+    @debug_print('Удалить все фильтры')
     def remove_filters(self, query: Query):
+        self.state = DialogState.filter
         allowed_states = (DialogState.filter, DialogState.start)
-        print('удалить все фильтры')
-
-    def count_filter(self, query: Query):
-        allowed_states = (DialogState.filter, DialogState.start)
-        print(query.arguments.items())
-        print('количественный фильтр')
 
     def unknown(self):
         print('Я не понял вопрос')
 
-    def solve(self, query: Query):
-        query_structure = query.query_structure
+    ##
+    def find_like_dislike_patterns(self, query: Query):
+        tags = query.tags_query_structure
+        if 'dislike' in tags and 'exclude' in tags:
+            self.like(query)
+            return 'like'
+        if 'dislike' in tags:
+            self.dislike(query)
+            return 'dislike'
+        if 'like' in tags and 'exclude' in tags:
+            self.dislike(query)
+            return 'dislike'
+        if 'like' in tags:
+            self.like(query)
+            return 'like'
+
+    def find_number_query_patterns(self, query: Query):
         tags = query.tags_query_structure
         arguments = query.arguments
-        if 'restart' in query_structure:
+        if ('number' in tags or 'how many' in tags) and 'SexArgument' in arguments:
+            self.number_with_sex(query)
+            return 'number_with_sex'
+        if ('number' in tags or 'how many' in tags) and ('NumArgument' in arguments or 'older' in tags or 'younger' in tags):
+            self.number_with_age(query)
+            return 'number_with_age'
+        if 'number' in tags or 'how many' in tags:
+            self.number(query)
+            return 'number'
+
+    def find_search_patterns(self, query: Query):
+        tags = query.tags_query_structure
+        arguments = query.arguments
+        if 'SexArgument' in arguments and ('filter' in tags or 'artist' in tags):
+            self.search_by_sex(query)
+            return 'search_by_sex'
+        if 'genre' in tags and 'GenreArgument' in arguments:
+            self.search_by_genre(query)
+            return 'search_by_genre'
+        if 'search' in tags and 'ArtistArgument' in arguments:
+            self.search_by_artist(query)
+            return 'search_by_artist'
+        if 'all' in tags and ('include' in tags or 'artist' in tags):
+            self.show_all_artists(query)
+            return 'show_all_artists'
+
+    def find_info_patterns(self, query: Query):
+        tags = query.tags_query_structure
+        arguments = query.arguments
+        if 'ArtistArgument' in arguments and ('show' in tags or 'about' in tags or 'info' in tags):
+            self.info(query)
+            return 'info'
+
+    def find_filter_patterns(self, query: Query):
+        tags = query.tags_query_structure
+        arguments = query.arguments
+        if 'SexArgument' in arguments and ('filter' in tags or 'artist' in tags):
+            self.filter_by_sex(query)
+            return 'filter_by_sex'
+        if ('older' in tags or 'younger' in tags) and 'include' in tags:
+            self.filter_by_age_include(query)
+            return 'filter_by_age_include'
+        if ('older' in tags or 'younger' in tags) and 'exclude' in tags:
+            self.filter_by_age_exclude(query)
+            return 'filter_by_age_exclude'
+        if 'include' in tags and 'NumArgument' in arguments:
+            self.set_result_len(query)
+            return 'set_result_len'
+        if 'group' in tags or 'solo' in tags or 'duet' in tags:
+            self.filter_by_members_count(query)
+            return 'filter_by_members_count'
+        if 'exclude' in tags and 'all' in tags:
+            self.remove_filters(query)
+            return 'remove_filters'
+        if ('exclude' in tags or 'include' in tags) and ('all' in tags or 'number' in tags):
+            self.remove_result_len_filter(query)
+            return 'remove_result_len_filter'
+
+    def solve(self, query: Query):
+        tags = query.tags_query_structure
+        if 'restart' in tags:
             self.restart()
             return 'restart'
-        if self.state == DialogState.start:
-            if 'dislike' in query_structure and 'exclude' in tags:
-                self.like(query)
-                return 'like'
-            if 'dislike' in query_structure:
-                self.dislike(query)
-                return 'dislike'
-            if 'like' in query_structure and 'exclude' in tags:
-                self.dislike(query)
-                return 'dislike'
-            if 'like' in query_structure:
-                self.like(query)
-                return 'like'
-            if 'number' in query_structure and 'SexArgument' in arguments:
-                self.number_with_sex(query)
-                return 'number_with_sex'
-            if 'number' in query_structure and ('NumArgument' in arguments or 'older' in tags or 'younger' in tags):
-                self.number_with_age(query)
-                return 'number_with_age'
-            if 'number' in query_structure:
-                self.number(query)
-                return 'number'
-            if 'SexArgument' in arguments and ('filter' in query_structure or 'artist' in tags):
-                self.search_by_sex(query)
-                return 'search_by_sex'
-            if 'search' in query_structure and 'GenreArgument' in arguments:
-                self.search_by_genre(query)
-                return 'search_by_genre'
-            if 'search' in query_structure and 'ArtistArgument' in arguments:
-                self.search_by_artist(query)
-                return 'search_by_artist'
-            if 'search' in query_structure and 'all' in tags:
-                self.show_all_artists(query)
-                return 'show_all_artists'
-            if 'info' in query_structure:
-                self.info(query)
-                return 'info'
+        if self.state in (DialogState.search, DialogState.filter, DialogState.count_filter):
+            res = self.find_filter_patterns(query)
+            if res:
+                return res
             else:
-                self.unknown()
-        elif self.state == DialogState.search or self.state == DialogState.filter:
-            if 'SexArgument' in arguments and ('filter' in query_structure or 'artist' in tags):
-                self.filter_by_sex(query)
-                return 'filter_by_sex'
-            if 'filter' in query_structure and ('older' in tags or 'younger' in tags) and 'include' in tags:
-                self.filter_by_age_include(query)
-                return 'filter_by_age_include'
-            if 'filter' in query_structure and ('older' in tags or 'younger' in tags) and 'exclude' in tags:
-                self.filter_by_age_exclude(query)
-                return 'filter_by_age_exclude'
-            if 'filter' in query_structure and 'NumArgument' in arguments:
-                self.set_result_len(query)
-                return 'set_result_len'
-            if 'filter' in query_structure and ('group' in tags or 'solo' in tags or 'duet' in tags):
-                self.filter_by_members_count(query)
-                return 'filter_by_members_count'
-            if 'filter' in query_structure and 'exclude' in tags and 'all' in tags:
-                self.remove_filters(query)
-                return 'remove_filters'
-            if 'filter' in query_structure and ('all' in tags or 'exclude' in tags or 'number' in tags):
-                self.remove_result_len_filter(query)
-                return 'remove_result_len_filter'
-            if 'count_filter' in query_structure:
-                self.count_filter(query)
-                return 'count_filter'
-            else:
-                self.unknown()
+                self.state = DialogState.start
+
+        if self.state in (DialogState.start, DialogState.number, DialogState.like, DialogState.dislike, DialogState.info):
+            res = self.find_like_dislike_patterns(query)
+            if res: return res
+
+            res = self.find_number_query_patterns(query)
+            if res: return res
+
+            res = self.find_search_patterns(query)
+            if res: return res
+
+            res = self.find_info_patterns(query)
+            if res: return res
+
+        self.unknown()
+
+
 
 
 
