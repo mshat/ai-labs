c689d37d0c06a4e50fe3ad3e6c21e5e9bc356187
Maksim Shatokhin <max.sh99@mail.ru>
2022-02-22 20:10:45 +0300

    Добавил поля-фильры в User

--- lab8/src/user.py
+++ lab8/src/user.py
@@ -1,28 +1,80 @@
-from typing import List
+from typing import List, Dict
+from const import SexFilter, GroupTypeFilter
 
 
 class User:
-    likes: List[str]
-    dislikes: List[str]
+    _name: str
+    _likes: List[str]
+    _dislikes: List[str]
+    _group_type_filter: GroupTypeFilter
+    _younger_filter: int | None
+    _older_filter: int | None
+    _sex_filter: SexFilter
 
     def __init__(self, name: str = None):
-        self.name = name
-        self.likes = []
-        self.dislikes = []
+        self._name = name
+        self._likes = []
+        self._dislikes = []
+        self._group_type_filter = GroupTypeFilter.any
+        self._younger_filter = None
+        self._older_filter = None
+        self._sex_filter = SexFilter.any
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def likes(self) -> List[str]:
+        return self._likes
+
+    @property
+    def dislikes(self) -> List[str]:
+        return self._dislikes
+
+    @property
+    def group_type_filter(self) -> GroupTypeFilter:
+        return self._group_type_filter
+
+    @property
+    def younger_filter(self) -> int | None:
+        return self.younger_filter
+
+    @younger_filter.setter
+    def younger_filter(self, val: int):
+        self._younger_filter = val
+
+    @property
+    def older_filter(self) -> int | None:
+        return self.older_filter
+
+    @older_filter.setter
+    def older_filter(self, val: int):
+        self._older_filter = val
+
+    @property
+    def sex_filter(self) -> SexFilter | None:
+        return self._sex_filter
+
+    def add_group_type_filter(self, filter_: GroupTypeFilter):
+        self._group_type_filter = filter_
+
+    def add_sex_filter(self, sex: SexFilter):
+        self._sex_filter = sex
 
     def add_like(self, artist_name: str):
-        if artist_name in self.dislikes:
-            self.dislikes.remove(artist_name)
+        if artist_name in self._dislikes:
+            self._dislikes.remove(artist_name)
         else:
-            if artist_name not in self.likes:
-                self.likes.append(artist_name)
+            if artist_name not in self._likes:
+                self._likes.append(artist_name)
 
     def add_dislike(self, artist_name: str):
-        if artist_name in self.likes:
-            self.likes.remove(artist_name)
+        if artist_name in self._likes:
+            self._likes.remove(artist_name)
         else:
-            if artist_name not in self.dislikes:
-                self.dislikes.append(artist_name)
+            if artist_name not in self._dislikes:
+                self._dislikes.append(artist_name)
 
     def __str__(self):
-        return f'User {self.name} likes: {self.likes}, dislikes: {self.dislikes}'
+        return f'User {self.name} likes: {self._likes}, dislikes: {self._dislikes}'
