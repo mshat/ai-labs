74bf80816957bc8fc87a9b5c628732e079c17a83
Maksim Shatokhin <max.sh99@mail.ru>
2022-02-22 17:52:40 +0300

    перенёс и пофиксил тесты test_conditions.py

--- lab8/src/tests/test_conditions.py
+++ lab8/src/tests/test_conditions.py
@@ -1,35 +1,35 @@
 import unittest
 from unittest.mock import Mock
-from tag_condition import (AndMultiTagCondition, OrMultiTagCondition, AndTagCondition, OrTagCondition, TagCondition,
+from lab8.src.tag_condition import (AndMultiTagCondition, OrMultiTagCondition, AndTagCondition, OrTagCondition, TagCondition,
                            MultiTagCondition, PatternMatcher)
 
 
 class TestTagSimpleCondition(unittest.TestCase):
-    tags = {'search': None}
+    tags = {'search': ''}
 
     def test_tag_condition_pos(self):
         condition = AndTagCondition('search')
 
-        res = condition.solve(self.tags)
+        res = condition.solve(self.tags)[0]
 
         self.assertTrue(res)
 
     def test_tag_condition_neg(self):
         condition = AndTagCondition('artist')
 
-        res = condition.solve(self.tags)
+        res = condition.solve(self.tags)[0]
 
         self.assertFalse(res)
 
 
 class TestQueryPatternWithSimpleConditions(unittest.TestCase):
-    query_mock = Mock(query_tag_structure={'search': None}, arguments={'ArtistArgument': None})
+    query_mock = Mock(query_tag_structure={'search': ''}, arguments={'ArtistArgument': ''})
 
     def test_one_AND_condition(self):
         condition = AndTagCondition('search')
         pattern_matcher = PatternMatcher(conditions=[condition])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
 
@@ -37,17 +37,17 @@ class TestQueryPatternWithSimpleConditions(unittest.TestCase):
         condition = OrTagCondition('search')
         pattern_matcher = PatternMatcher(conditions=[condition])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
 
     def test_two_AND_conditions_pos(self):
         condition1 = AndTagCondition('search')
         condition2 = AndTagCondition('artist')
-        self.query_mock.query_tag_structure = {'search': None, 'artist': None}
+        self.query_mock.query_tag_structure = {'search': '', 'artist': ''}
         pattern_matcher = PatternMatcher(conditions=[condition1, condition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
 
@@ -55,10 +55,10 @@ class TestQueryPatternWithSimpleConditions(unittest.TestCase):
         """ Паттерн не подойдёт, тк условие 2 будет ложным (в запросе нет тэга artist)"""
         condition1 = AndTagCondition('search')
         condition2 = AndTagCondition('artist')
-        self.query_mock.query_tag_structure = {'search': None}
+        self.query_mock.query_tag_structure = {'search': ''}
         pattern_matcher = PatternMatcher(conditions=[condition1, condition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertFalse(res)
 
@@ -69,7 +69,7 @@ class TestQueryPatternWithSimpleConditions(unittest.TestCase):
         self.query_mock.query_tag_structure = {}
         pattern_matcher = PatternMatcher(conditions=[condition1, condition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertFalse(res)
 
@@ -77,10 +77,10 @@ class TestQueryPatternWithSimpleConditions(unittest.TestCase):
         """ Паттерн подойдёт, несмотря на то, что условие 2 будет ложным. Так как условие 2 - ИЛИ"""
         condition1 = AndTagCondition('search')
         condition2 = OrTagCondition('artist')
-        self.query_mock.query_tag_structure = {'search': None}
+        self.query_mock.query_tag_structure = {'search': ''}
         pattern_matcher = PatternMatcher(conditions=[condition1, condition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
 
@@ -88,10 +88,10 @@ class TestQueryPatternWithSimpleConditions(unittest.TestCase):
         """ Паттерн подойдёт, несмотря на то, что условие 2 будет ложным. Так как оба условия - ИЛИ"""
         condition1 = OrTagCondition('search')
         condition2 = OrTagCondition('artist')
-        self.query_mock.query_tag_structure = {'search': None}
+        self.query_mock.query_tag_structure = {'search': ''}
         pattern_matcher = PatternMatcher(conditions=[condition1, condition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
 
@@ -102,13 +102,13 @@ class TestQueryPatternWithSimpleConditions(unittest.TestCase):
         self.query_mock.query_tag_structure = {}
         pattern_matcher = PatternMatcher(conditions=[condition1, condition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertFalse(res)
 
 
 class TestQueryPatternWithMultiConditions(unittest.TestCase):
-    query_mock = Mock(query_tag_structure={'search': None, 'artist': None}, arguments={'ArtistArgument': None})
+    query_mock = Mock(query_tag_structure={'search': '', 'artist': ''}, arguments={'ArtistArgument': ''})
 
     def test_one_multicondition_pos(self):
         condition1 = AndTagCondition('search')
@@ -116,7 +116,7 @@ class TestQueryPatternWithMultiConditions(unittest.TestCase):
         multicondition = AndMultiTagCondition(conditions=[condition1, condition2])
         pattern_matcher = PatternMatcher(conditions=[multicondition])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
 
@@ -126,7 +126,7 @@ class TestQueryPatternWithMultiConditions(unittest.TestCase):
         multicondition = AndMultiTagCondition(conditions=[condition1, condition2])
         pattern_matcher = PatternMatcher(conditions=[multicondition])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertFalse(res)
 
@@ -136,9 +136,9 @@ class TestQueryPatternWithMultiConditions(unittest.TestCase):
         multicondition1 = AndMultiTagCondition(conditions=[condition1, condition2])
         multicondition2 = AndMultiTagCondition(conditions=[condition1, condition2])
 
-        self.query_mock.query_tag_structure = {'search': None, 'artist': None}
+        self.query_mock.query_tag_structure = {'search': '', 'artist': ''}
         pattern_matcher = PatternMatcher(conditions=[multicondition1, multicondition2])
 
-        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)
+        res = pattern_matcher.match_pattern(self.query_mock.query_tag_structure)[0]
 
         self.assertTrue(res)
