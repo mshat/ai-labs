4d19f9520294af09a01616ff22114e00bbb029d0
Maksim Shatokhin <max.sh99@mail.ru>
2022-02-22 20:08:06 +0300

    добавил filter_artists, зарефакторил filter_ и переименовал в filter_recommendations

--- lab8/src/recommender_system/filter.py
+++ lab8/src/recommender_system/filter.py
@@ -9,18 +9,31 @@ def loosen_filters(filtered_recommendations, values, recommendations):
     exclude = []
     while not filtered_recommendations and i < len(EXCLUDE):
         exclude.append(EXCLUDE[i])
-        filtered_recommendations = filter_(parse_filters(values), recommendations, exclude=exclude)
+        filters = parse_filters(values)
+        group_type = filters['group_type']
+        sex = filters['sex']
+        older = int(filters['older']) if filters['older'] else None
+        younger = int(filters['younger']) if filters['younger'] else None
+        filtered_recommendations = filter_recommendations(
+            recommendations,
+            group_type=group_type,
+            sex=sex,
+            younger=younger,
+            older=older,
+            exclude=exclude)
         i += 1
     return filtered_recommendations
 
 
-def filter_(filters, recommendations, exclude=None):
+def filter_recommendations(
+        recommendations,
+        group_type: str = 'any',
+        sex: str = 'anysex',
+        younger: int = None,
+        older: int = None,
+        exclude=None):
     exclude = exclude if exclude else []
     filtered_recommendations = OrderedDict()
-    group_type = filters['group_type']
-    sex = filters['sex']
-    older = int(filters['older']) if filters['older'] else None
-    younger = int(filters['younger']) if filters['younger'] else None
     for artist_name, proximity in recommendations.items():
         artist = find_artist(artist_name)
         if group_type != 'any' and artist.solo_duet_group != group_type and 'group_type' not in exclude:
@@ -34,6 +47,29 @@ def filter_(filters, recommendations, exclude=None):
         if artist_name not in filtered_recommendations:
             filtered_recommendations.update({artist_name: proximity})
     return filtered_recommendations
+
+
+def filter_artists(
+        artists,
+        group_type: str = 'any',
+        sex: str = 'anysex',
+        younger: int = None,
+        older: int = None,
+        exclude=None):
+    exclude = exclude if exclude else []
+    filtered = []
+    for artist in artists:
+        if group_type != 'any' and artist.solo_duet_group != group_type and 'group_type' not in exclude:
+            continue
+        if sex != 'anysex' and artist.sex != sex and 'sex' not in exclude:
+            continue
+        if older and artist.age <= older and 'older' not in exclude:
+            continue
+        if younger and artist.age >= younger and 'younger' not in exclude:
+            continue
+        if artist not in filtered:
+            filtered.append(artist)
+    return filtered
 
 
 def parse_filters(values) -> dict:
