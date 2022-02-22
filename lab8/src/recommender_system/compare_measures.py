f979f7959e2c37318010478e452af7ea4cc29bb6
Maksim Shatokhin <max.sh99@mail.ru>
2022-02-22 17:54:07 +0300

    переименовал get_leafs в get_leafs_values

--- lab8/src/recommender_system/compare_measures.py
+++ lab8/src/recommender_system/compare_measures.py
@@ -1,4 +1,4 @@
-from tree.tree_tools import get_leafs, calc_max_distance_between_nodes_optimized, calc_distance_between_all_nodes
+from tree.tree_tools import get_leafs_values, calc_max_distance_between_nodes_optimized, calc_distance_between_all_nodes
 from proximity_measures import calc_euclidean_measure, calc_manhattan_measure, generalizing_proximity_measure
 from proximity_measures import calc_tree_distance_measure
 from tools import format_print
@@ -11,7 +11,7 @@ def compare_measures(tree, artist_pairs, min_general_proximity: float = None, ma
     generalizing_proximity = []
 
     leafs = []
-    get_leafs(tree, leafs)
+    get_leafs_values(tree, leafs)
 
     distances_between_nodes = calc_distance_between_all_nodes(tree, leafs)
     max_distance_between_artists = calc_max_distance_between_nodes_optimized(distances_between_nodes)
